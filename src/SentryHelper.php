c63db8e85f3334bfdbecfa1b029a27f4901e9d72
Bohdan <batievbt@gmail.com>
2019-02-13 12:30:14 +0000

    HC-3402 Added Sentry lib

---
+++ src/SentryHelper.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace HelpcrunchSentry;
+
+class SentryHelper
+{
+    /**
+     * @var \Raven_Client
+     */
+    private static $ravenClient;
+
+    /**
+     * @param string|null $ravenUrl
+     * @throws \Raven_Exception
+     */
+    public static function install(string $ravenUrl = null): void
+    {
+        if (!$ravenUrl) {
+            if (!empty($_SERVER['RAVEN_URL'])) {
+                $ravenUrl = $_SERVER['RAVEN_URL'];
+            } else {
+                throw new \InvalidArgumentException('No raven url provided');
+            }
+        }
+
+        self::$ravenClient = new Raven_Client($ravenUrl);
+        self::$ravenClient->install($ravenUrl);
+    }
+
+    public static function log($message): void
+    {
+        if (!self::$ravenClient) {
+            return;
+        }
+
+        if (is_array($message) || is_object($message)) {
+            $message = json_encode($message);
+        }
+
+        self::$ravenClient->captureMessage($message, ['log'], ['level' => \Raven_Client::DEBUG]);
+    }
+}
